b2ee33fa57201a4be238f5945cfd3a2fa26e4255
Leask Huang <i@leaskh.com>
2015-01-06 16:56:00 +0800

    [22.6066209865756,114.125673658894] improved memo package

--- m
+++ m
@@ -25,6 +25,73 @@ var sqlite = require('sqlite3').verbose();
 
 // db init
 
+var getAllMemos = function(callback) {
+    var db = new sqlite.Database(env.dbFile);
+    db.all(
+        'SELECT * FROM `memos` WHERE '
+      + "`status` != 'DELETED' "
+      + 'ORDER BY `updated_at` DESC',
+        function(err, data) {
+            db.close();
+            if (err) {
+                console.log(':( ' + err);
+                process.exit(1);
+            }
+            callback(data);
+        }
+    );
+};
+
+var searchForMemos = function(keyword, callback) {
+    var db = new sqlite.Database(env.dbFile);
+    db.all(
+        'SELECT * FROM `memos` WHERE '
+      + "`memo` LIKE '%keyword%'",
+        function(err, data) {
+            db.close();
+            if (err) {
+                console.log(':( ' + err);
+                process.exit(1);
+            }
+            callback(data);
+        }
+    );
+};
+
+var getMemoById = function(id, callback) {
+    var db = new sqlite.Database(env.dbFile);
+    db.get(
+        'SELECT * FROM `memos` WHERE `id` = ?',
+        id,
+        function(err, data) {
+            db.close();
+            if (err) {
+                console.log(':( ' + err);
+                process.exit(1);
+            }
+            callback(data);
+        }
+    );
+};
+
+var newMemo = function(memo, callback) {
+    var db = new sqlite.Database(env.dbFile);
+    db.run(
+        'INSERT INTO `memos` '
+      + '(`memo`, `created_at`, `updated_at`, `deleted_at`, `status`, `first_matched`, `last_matched`, `hits`) '
+      + 'VALUES '
+      + "(?, datetime('now'), datetime('now'), datetime('now'), 'NORMAL', datetime('now'), datetime('now'), 1)",
+        memo,
+        function(err) {
+            db.close();
+            if (err) {
+                console.log(':( ' + err);
+                process.exit(1);
+            }
+            getMemoById(this.lastID, callback);
+        }
+    );
+};
 
 var touchMemo = function(memo, callback) {
 
--- memo.js
+++ memo.js
@@ -1,102 +1,77 @@
 'use strict';
 
-// load requirements
-var sqlite = require('sqlite3').verbose();
-
 var memo = function(env) {
 
-    var _getById = function(req, res) {
+    // load requirements
+    var _sqlite = require('sqlite3').verbose();
+
+    var _db = null;
+
+    var _init = function() {
+        // db init
+        if (!_db) {
+            _db = new sqlite.Database(env.dbFile);
+        }
+    };
+
+    var _del = function() {
+        if (_db) {
+            _db.close();
+        }
+    }
+
+    var _getAll = function(callback) {
+        _db.all(
+            'SELECT * FROM `memos` WHERE '
+          + "`status` != 'DELETED' "
+          + 'ORDER BY `updated_at` DESC',
+            callback
+        );
+    };
+
+    var _searchByKeyword = function(keyword, callback) {
+        _db.all(
+            'SELECT * FROM `memos` WHERE '
+          + "`memo` LIKE '%keyword%'",
+            callback
+        );
+    };
+
+    var _getById = function(id, callback) {
+        _db.get(
+            'SELECT * FROM `memos` WHERE `id` = ?',
+            id,
+            callback
+        );
+    };
+
+    var _add = function(memo, callback) {
+        db.run(
+            'INSERT INTO `memos` '
+          + '(`memo`, `created_at`, `updated_at`, `deleted_at`, `status`, `first_matched`, `last_matched`, `hits`) '
+          + 'VALUES '
+          + "(?, datetime('now'), datetime('now'), datetime('now'), 'NORMAL', datetime('now'), datetime('now'), 1)",
+            memo,
+            function(err) {
+                if (err) {
+                    return callback(err);
+                }
+                getMemoById(this.lastID, callback);
+            }
+        );
+    };
+
+    var _touch = function(memo, callback) {
 
     };
 
-
-
-
-
-
-var getAllMemos = function(callback) {
-    var db = new sqlite.Database(env.dbFile);
-    db.all(
-        'SELECT * FROM `memos` WHERE '
-      + "`status` != 'DELETED' "
-      + 'ORDER BY `updated_at` DESC',
-        function(err, data) {
-            db.close();
-            if (err) {
-                console.log(':( ' + err);
-                process.exit(1);
-            }
-            callback(data);
-        }
-    );
-};
-
-var searchForMemos = function(keyword, callback) {
-    var db = new sqlite.Database(env.dbFile);
-    db.all(
-        'SELECT * FROM `memos` WHERE '
-      + "`memo` LIKE '%keyword%'",
-        function(err, data) {
-            db.close();
-            if (err) {
-                console.log(':( ' + err);
-                process.exit(1);
-            }
-            callback(data);
-        }
-    );
-};
-
-var getMemoById = function(id, callback) {
-    var db = new sqlite.Database(env.dbFile);
-    db.get(
-        'SELECT * FROM `memos` WHERE `id` = ?',
-        id,
-        function(err, data) {
-            db.close();
-            if (err) {
-                console.log(':( ' + err);
-                process.exit(1);
-            }
-            callback(data);
-        }
-    );
-};
-
-var newMemo = function(memo, callback) {
-    var db = new sqlite.Database(env.dbFile);
-    db.run(
-        'INSERT INTO `memos` '
-      + '(`memo`, `created_at`, `updated_at`, `deleted_at`, `status`, `first_matched`, `last_matched`, `hits`) '
-      + 'VALUES '
-      + "(?, datetime('now'), datetime('now'), datetime('now'), 'NORMAL', datetime('now'), datetime('now'), 1)",
-        memo,
-        function(err) {
-            db.close();
-            if (err) {
-                console.log(':( ' + err);
-                process.exit(1);
-            }
-            getMemoById(this.lastID, callback);
-        }
-    );
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     return {
-        getById : _getById
+        Init            : _init,
+        Del             : _del,
+        GetAll          : _getAll,
+        SearchByKeyword : _searchByKeyword,
+        GetById         : _getById,
+        Add             : _add
     };
 
 }();
